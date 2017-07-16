38e8c472fd40ef3b82de022dc6a952e53fe9324c
AntonShelest <shelest.anton@gmail.com>
2017-07-16 13:37:40 +0300

    Update Manager.java

--- src/app/Manager.java
+++ src/app/Manager.java
@@ -21,7 +21,7 @@ public class Manager {
     public void startProjects() {
         for(Project project : projects){
             try {
-                project.assignRowersToProject(bench);
+                project.assignRowersToProject(bench); // better to rename to 'assignRowers(...)'
             } catch (NotEnoughRowersException e) {
                 e.printStackTrace();
             }
@@ -30,7 +30,7 @@ public class Manager {
     }
 
     public void initRower(){
-        bench.addRower(3.0, 56);
+        bench.addRower(3.0, 56); // I would initialize bench and projects from file
         bench.addRower(2.5, 65);
         bench.addRower(2.1, 55);
         bench.addRower(3.5, 10);
